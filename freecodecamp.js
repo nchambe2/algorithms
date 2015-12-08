20da782a8a12f8a5ac21dcdeb483ac8f86af8ab0
Nicolette Chambers <nicolette3883@gmail.com>
2015-12-08 16:48:22 -0600

    Create chunk method

--- freecodecamp.js
+++ freecodecamp.js
@@ -163,9 +163,9 @@ repeat("abc", -2);
 
 // INPUT: Obtain a array and a number
 // STEPS:
-// Create a empty container object called split array and set it eql to splitArray
+// Create a empty container object called split array and set it eql to []
 // Iterate through the array n amount of times
-//    Push each number into the new array
+//    Push each number into the new array whose index is less than the size
 // ELSE
 //    return the original array
 // OUTPUT: Return the string split into subarrays based on the number in the argument
@@ -174,8 +174,12 @@ function chunk(arr, size) {
     var splitArray = [];
 
     for (var i = 0; i < arr.length ; i++) {
-        if (i >= size)>
+            if (arr.index(arr[i]) < size) {
+              splitArray.push(arr[i]);
+            }
     }
+
+    return splitArray;
 }
 
 console.log(chunk(["a", "b", "c", "d"], 2));
@@ -183,22 +187,3 @@ console.log(chunk([0, 1, 2, 3, 4, 5], 3));
 console.log(chunk([0, 1, 2, 3, 4, 5], 2));
 console.log(chunk([0, 1, 2, 3, 4, 5], 4));
 
-function Dog(name, breed, weight) {
-    this.name =  name;
-    this.breed = breed;
-    this.weight = weight;
-}
-
-var fido = new Dog("Fido", "Mixed", 38);
-var fluffy = new Dog("Fluffy", "Poodle", 30);
-var spot = new Dog("Spot", "Chihuahua", 10);
-var dogs = [fido, fluffy, spot];
-
-for (var i = 0; i < dogs.length; i++) {
-  var size =
-}
-
-function Duck(type, canFly) {
-    this.type = type;
-    this.canFly = true;
-}
